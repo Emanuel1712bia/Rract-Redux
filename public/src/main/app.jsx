2f40401cde2f4815e90dad8180145f636780fefb
Emanuel Nascimento <lel_emanuel@hotmail.com>
2020-08-29 14:57:42 -0300

    Erro

---
+++ public/src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/fonr-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Emanuel</h1>
+    </div>
+)
